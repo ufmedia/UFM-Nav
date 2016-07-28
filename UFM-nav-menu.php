2b146df2ffb312e0e2e9a6bf6c419f52e04ae0d5
ufmedia <john@ufmedia.co.uk>
2016-07-28 13:28:17 +0100

    Basic Bootstrap nav with dropdown

---
+++ UFM-nav-menu.php
@@ -0,0 +1,87 @@
+<?php
+/*
+Plugin Name: UFM Nav Menu
+Description: This plugin builds a Bootstrap nav from the page structure within WP.
+Version: 1.0.0
+Author: John Thompson
+Author URI: http://www.ufmedia.net
+*/
+
+function get_nav() {
+$currentID = get_the_ID();
+
+$args = array(
+
+			'post_type' => 'page',
+			'numberposts' => 100,
+			'orderby' => 'menu_order',
+			'order' => 'ASC',
+			'post_parent' => 0,
+			'meta_query' => array(
+				array(
+					'key' => 'hide_from_nav',
+					'value' => '"1"',
+					'compare' => 'NOT LIKE'
+					
+				) 
+			)
+			
+			);
+	
+	
+$pages = get_posts($args);
+$navHTML .= '<ul class="nav">';
+foreach ($pages as $page) {
+
+$args = array(
+
+			'post_type' => 'page',
+			'numberposts' => 100,
+			'orderby' => 'menu_order',
+			'order' => 'ASC',
+			'post_parent' => $page->ID,
+			'meta_query' => array(
+				array(
+					'key' => 'hide_from_nav',
+					'value' => '1',
+					'compare' => '!='
+					
+				) 
+			)
+			);
+
+$children = get_posts($args);
+
+if( count( $children ) != 0 ) { 
+    
+	$navHTML .= '<li class="dropdown"><a href="#" data-toggle="dropdown">' . get_the_title($page->ID) . '<span class="drop-caret"></span></a>';
+	$navHTML .= '<ul class="dropdown-menu">';
+	foreach ($children as $child) {
+		
+	$navHTML .= '<li';
+	if ($child->ID == $currentID) {
+		$navHTML .= ' class="active"';
+	}
+	$navHTML .= '><a href="' . get_permalink($child->ID) . '"><span>' . get_the_title($child->ID) . '</span></a></li>';
+		
+	}
+	$navHTML .= '</ul></li>';
+ 
+} else {
+
+	
+	
+	$navHTML .= '<li';
+	if ($page->ID == $currentID) {
+		$navHTML .= ' class="active"';
+	}
+	$navHTML .= '><a href="' . get_permalink($page->ID) . '">' . get_the_title($page->ID) . '</a></li>';
+
+	}
+}
+
+$navHTML .= '</ul>';
+
+return $navHTML;	
+
+}
